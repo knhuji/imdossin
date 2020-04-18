9f7bc9a1b198693300523e891558b8f7278e85ff
knhuji <kimnhu10tn1@gmail.com>
2020-04-18 22:23:40 +0700

    Data product

---
+++ imdossin/Migrations/20200418144822_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace imdossin.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Prod_ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Prod_Name = table.Column<string>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    Size = table.Column<string>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Supplier = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Image = table.Column<byte[]>(nullable: true),
+                    UrlImage = table.Column<string>(nullable: true),
+                    Status_ID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Prod_ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ imdossin/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imdossin.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Prod_ID { get; set; }
+        [Required]
+        public string Prod_Name { get; set; }
+        public int Price { get; set; }
+        public string Size { get; set; }
+        public int Quantity { get; set; } //Số lượng
+        public string Supplier { get; set; } //Nhà cung cấp
+        public DateTime Date { get; set; }
+        public string Description { get; set; } //Mô tả sp
+        public byte[] Image { get; set; } //Hình ảnh sp
+        public string UrlImage { get; set; }//Đường dẫn hình ảnh
+        public int Status_ID { get; set; }
+    }
+}
+
+
